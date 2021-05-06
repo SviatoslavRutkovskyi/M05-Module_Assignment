1ffc8c57493b03cf83f26b3cb77e264113bca56a
SviatoslavRutkovskyi <82433745+SviatoslavRutkovskyi@users.noreply.github.com>
2021-05-06 15:56:51 -0700

    Update multiplication.js
    prompt to pick a number and removing unnecessary things

--- multiplication.js
+++ multiplication.js
@@ -1,9 +1,11 @@
 // JavaScript source code
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
+var table = prompt("Please enter  a number from 0 to 10", "0");    // setting the unit of table
+// var table = 3;             
+var operator = 'multiplication'; // Type of calculation
 var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var msg = '<h2>Multiplication Table</h2>';              // Message
 
+/*
 if (operator === 'addition') {
     // Do addition
     while (i < 11) {
@@ -11,12 +13,13 @@ if (operator === 'addition') {
         i++;
     }
 } else {
+*/
     // Do multiplication
     while (i < 11) {
         msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
         i++;
     }
-}
+//}
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
